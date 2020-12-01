c4705323175837155d6ba88ac9a046893db4ca37
Cipto Hadi <ciptohadi79@gmail.com>
2020-12-01 18:55:48 +0700

    using useContect hook to acces multiple Context

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import ColorContext from "../contexts/ColorContext";
 import LanguageContext from "../contexts/LanguageContext";
 import UserCreate from "./UserCreate";
 
 const App = () => {
   const [language, setLanguage] = useState("english");
+  const [color, setColor] = useState("english");
 
   return (
     <div className="ui container">
@@ -12,9 +14,16 @@ const App = () => {
         <i className="flag us" onClick={() => setLanguage("english")} />
         <i className="flag nl" onClick={() => setLanguage("dutch")} />
       </div>
-      <LanguageContext.Provider value={language}>
-        <UserCreate />
-      </LanguageContext.Provider>
+      <div>
+        Select a button color:
+        <i className="red circle icon" onClick={() => setColor("red")} />
+        <i className="blue circle icon" onClick={() => setColor("primary")} />
+      </div>
+      <ColorContext.Provider value={color}>
+        <LanguageContext.Provider value={language}>
+          <UserCreate />
+        </LanguageContext.Provider>
+      </ColorContext.Provider>
     </div>
   );
 };
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,13 @@
 import { useContext } from "react";
+import ColorContext from "../contexts/ColorContext";
 import LanguageContext from "../contexts/LanguageContext";
 
 const Button = () => {
   const language = useContext(LanguageContext);
+  const color = useContext(ColorContext);
 
   const buttonText = language === "english" ? "Submit" : "Voorleggen";
-  return <button className="ui primary button">{buttonText}</button>;
+  return <button className={`ui ${color} button`}>{buttonText}</button>;
 };
 
 export default Button;

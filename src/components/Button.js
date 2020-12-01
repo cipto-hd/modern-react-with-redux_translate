8019c5365ace1acef11df5327d10c747012cdd58
Cipto Hadi <ciptohadi79@gmail.com>
2020-12-01 19:08:44 +0700

    using Context.Consumer to acces multiple Context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,28 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 import ColorContext from "../contexts/ColorContext";
 import LanguageContext from "../contexts/LanguageContext";
 
 const Button = () => {
-  const language = useContext(LanguageContext);
-  const color = useContext(ColorContext);
+  // const language = useContext(LanguageContext);
+  // const color = useContext(ColorContext);
+  // const buttonText = language === "english" ? "Submit" : "Voorleggen";
 
-  const buttonText = language === "english" ? "Submit" : "Voorleggen";
-  return <button className={`ui ${color} button`}>{buttonText}</button>;
+  const renderButtonText = (language) =>
+    language === "english" ? "Submit" : "Voorleggen";
+
+  const renderButton = (color) => (
+    <button className={`ui ${color} button`}>
+      <LanguageContext.Consumer>
+        {(language) => renderButtonText(language)}
+      </LanguageContext.Consumer>
+    </button>
+  );
+
+  return (
+    <ColorContext.Consumer>
+      {(color) => renderButton(color)}
+    </ColorContext.Consumer>
+  );
 };
 
 export default Button;
